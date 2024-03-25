77a5214cf148de0e56aa719f6254c8dfddd9bc53
adam bunce <74341873+adam-bunce@users.noreply.github.com>
2024-03-25 17:20:42 -0400

    Feat/customer stuff (#1)
    * stuff
    
    * add crud stuff
    
    * add

--- src/main/java/com/webdev/project/controller/CustomerController.java
+++ src/main/java/com/webdev/project/controller/CustomerController.java
@@ -1,4 +1,62 @@
 package com.webdev.project.controller;
 
+import com.webdev.project.model.Customer;
+import com.webdev.project.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customers")
 public class CustomerController {
+    CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        super();
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/list")
+    public String viewCustomers(Model model) {
+        model.addAttribute("customers", this.customerService.getAllCustomers());
+        return "/customers/list";
+    }
+
+    @GetMapping("/create")
+    public String viewCreateCustomer(Model model) {
+        model.addAttribute("newCustomer", new Customer());
+        return "/customers/create";
+    }
+
+    @PostMapping("/create")
+    public String createCustomer(Customer newCustomer, Model model) {
+        this.customerService.saveOrUpdateCustomer(newCustomer);
+        return "redirect:/customers/list";
+    }
+
+    @PostMapping("/delete")
+    public String deleteCustomer(Long customerId, Model model) {
+        this.customerService.deleteCustomerById(customerId);
+        return "redirect:/customers/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String viewEditCustomer(@PathVariable Long id, Model model) {
+        Customer customer = this.customerService.getCustomerById(id);
+        System.out.println(customer);
+        model.addAttribute("selectedCustomer", customer);
+        return "/customers/edit";
+    }
+
+    @PostMapping("/update")
+    public String updateCustomer(Customer customer, Model model) {
+        this.customerService.saveOrUpdateCustomer(customer);
+        return "redirect:/customers/list";
+    }
+
 }
--- src/main/java/com/webdev/project/service/CustomerService.java
+++ src/main/java/com/webdev/project/service/CustomerService.java
@@ -1,4 +1,35 @@
 package com.webdev.project.service;
 
+import com.webdev.project.model.Customer;
+import com.webdev.project.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+    CustomerRepo customerRepo;
+
+    @Autowired
+    public CustomerService(CustomerRepo repo) {
+        super();
+        this.customerRepo = repo;
+    }
+
+    public Customer saveOrUpdateCustomer(Customer customer) {
+        System.out.println(customer);
+        return this.customerRepo.save(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        return this.customerRepo.findAll();
+    }
+
+    public void deleteCustomerById(Long id) {
+        this.customerRepo.deleteById(id);
+    }
+
+    public Customer getCustomerById(Long id) {return this.customerRepo.getReferenceById(id);}
+
 }
