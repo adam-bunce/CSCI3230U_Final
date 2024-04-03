567ec24f570c4cebcd4824413e3fefbb55d822ce
ShyProton <shayanr1001@gmail.com>
2024-04-03 15:42:52 -0400

    adds boilerplate for navigating service stuff

--- src/main/java/com/webdev/project/controller/ProvidedServiceController.java
+++ src/main/java/com/webdev/project/controller/ProvidedServiceController.java
@@ -1,4 +1,61 @@
 package com.webdev.project.controller;
 
+import com.webdev.project.model.ProvidedService;
+import com.webdev.project.service.ProvidedServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/services")
 public class ProvidedServiceController {
+    ProvidedServiceService serviceService;
+
+    @Autowired
+    public ProvidedServiceController(ProvidedServiceService serviceService) {
+        super();
+        this.serviceService = serviceService;
+    }
+
+    @GetMapping("/list")
+    public String viewBookings(Model model) {
+        model.addAttribute("services", this.serviceService.getAllServices());
+        return "/services/list";
+    }
+
+    @GetMapping("/create")
+    public String viewCreateService(Model model) {
+        model.addAttribute("newService", new ProvidedService());
+        return "/services/create";
+    }
+
+    @PostMapping("/create")
+    public String createService(ProvidedService newService, Model model) {
+        this.serviceService.saveOrUpdateService(newService);
+        return "redirect:/services/list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteService(Long serviceId, Model model) {
+        this.serviceService.deleteByServiceId(serviceId);
+        return "redirect:/services/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String viewEditService(@PathVariable Long id, Model model) {
+        ProvidedService service = this.serviceService.getServiceById(id);
+        model.addAttribute("selectedService", service);
+
+        return "/services/edit";
+    }
+
+    @PostMapping("/update")
+    public String updateService(ProvidedService service, Model model) {
+        this.serviceService.saveOrUpdateService(service);
+        return "redirect:/services/list";
+    }
 }
--- src/main/java/com/webdev/project/service/ProvidedServiceService.java
+++ src/main/java/com/webdev/project/service/ProvidedServiceService.java
@@ -1,4 +1,34 @@
 package com.webdev.project.service;
 
+import com.webdev.project.model.ProvidedService;
+import com.webdev.project.repo.ProvidedServiceRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProvidedServiceService {
+    ProvidedServiceRepo serviceRepo;
+
+    @Autowired
+    public ProvidedServiceService(ProvidedServiceRepo serviceRepo) {
+        super();
+        this.serviceRepo = serviceRepo;
+    }
+
+    public ProvidedService saveOrUpdateService(ProvidedService service) {
+        return this.serviceRepo.save(service);
+    }
+
+    public List<ProvidedService> getAllServices() {
+        return this.serviceRepo.findAll();
+    }
+
+    public void deleteByServiceId(Long id) {
+        this.serviceRepo.deleteById(id);
+    }
+    public ProvidedService getServiceById(Long id) {
+        return this.serviceRepo.getReferenceById(id);
+    }
 }
